f95d27d330e1a76e2ce87bfef565fa9233209bde
haccht <haccht00@gmail.com>
2015-10-29 16:53:31 +0900

    Add support for deploying on windows

--- install.sh
+++ install.sh
@@ -9,10 +9,16 @@ do
 	ln -snfv $basedir/$dotfile $HOME/$dotfile
 done
 
-if [ ! -v $KEYHAC_DIR ]
+if [ $(uname -s) == "MSYS_NT-6.3" ]
 then
-	echo 
-	echo " [installing keyhac config.py]"
-	echo " $KEYHAC_DIR/config.py"
-	cp $basedir/keyhac/config.py $KEYHAC_DIR/config.py
+	if [ ! -v $KEYHAC_DIR ]
+	then
+		echo
+		echo " [installing keyhac config.py]"
+		echo " $KEYHAC_DIR/config.py"
+		ln -nfv $basedir/windows/keyhac/config.py $KEYHAC_DIR/config.py
+	fi
+
+	ln -nfv $basedir/windows/.gvimrc       $HOME/.gvimrc
+	ln -nfv $basedir/windows/.vimperatorrc $HOME/.vimperatorrc
 fi
