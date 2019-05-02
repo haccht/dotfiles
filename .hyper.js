e0d160248b0b4c51d401c65d71c3e0cac653db07
haccht <haccht@users.noreply.github.com>
2019-05-02 17:03:24 +0900

    Fix minor issue

--- .hyper.js
+++ .hyper.js
@@ -112,8 +112,8 @@ module.exports = {
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
     // by default ['--login'] will be used
-    //WSLshell: 'C:\\Windows\\System32\\bash.exe', shellArgs: ['--login'],
-    shell: '', shellArgs: ['~'],
+    //WSLshell: 'C:\\Windows\\System32\\bash.exe', shellArgs: ['~'],
+    shell: '', shellArgs: ['--login'],
 
     // for environment variables
     env: {},
