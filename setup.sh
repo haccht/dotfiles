a2aadf48330c3104d51f1f1f312e4934a91c0255
haccht <haccht@users.noreply.github.com>
2019-11-05 20:07:45 +0900

    WIP: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,93 +1,72 @@
 #! /bin/bash
 
-confirm () {
-  message=$1
-  while :
-  do
-    echo -n "${message} [y/N]: "
-    read answer
-    case $answer in
-      [yY]) return 0 ;;
-      *)    return 1 ;;
-    esac
-  done
-}
+set -eu
 
-installed () {
-  type $1 >/dev/null 2>&1
-  if [ $? -ne 0 ]; then
-    echo "$1 not installed"
-    exit 1
+UPDATE_BIN=0
+GO_VERSION=go1.12.4.linux-amd64
+
+install_package() {
+  pkgname=$1
+
+  if ${UPDATE_BIN} -eq 1 || type "${pkgname}" >/dev/null 2>&1; then
+    type yum      >/dev/null 2>&1 && sudo yum install -y "${pkgname}"
+    type yay      >/dev/null 2>&1 && sudo yay -S --noconfirm "${pkgname}"
+    type pacman   >/dev/null 2>&1 && sudo pacman -S --noconfirm "${pkgname}"
+    type apt-get  >/dev/null 2>&1 && sudo apt-get update && apt-get install -y
   fi
 }
 
-symlink () {
-  mkdir -p $(dirname "$2")
-  rm -f "$2"
-  ln -snfv "$1" "$2"
-}
+while getopts u OPT
+do
+  case $OPT in
+    "u" ) UPDATE_BIN=1 ;;
+  esac
+done
 
-cwd=$(pwd)
-mkdir -p "$HOME/bin"
-
-symlink "$cwd/.bash_profile" "$HOME/.bash_profile"
-symlink "$cwd/.bashrc"       "$HOME/.bashrc"
-symlink "$cwd/.colorrc"      "$HOME/.colorrc"
-symlink "$cwd/.gitconfig"    "$HOME/.gitconfig"
-symlink "$cwd/.gitignore"    "$HOME/.gitignore"
-symlink "$cwd/.tmux.conf"    "$HOME/.tmux.conf"
-symlink "$cwd/.vimrc"        "$HOME/.vimrc"
-symlink "$cwd/.gemrc"        "$HOME/.gemrc"
-symlink "$cwd/.irbrc"        "$HOME/.irbrc"
+ln -sfv "${PWD}/.bash_profile"   "${HOME}/.bash_profile"
+ln -sfv "${PWD}/.bashrc"         "${HOME}/.bashrc"
+ln -sfv "${PWD}/.colorrc"        "${HOME}/.colorrc"
+ln -sfv "${PWD}/.gitconfig"      "${HOME}/.gitconfig"
+ln -sfv "${PWD}/.gitignore"      "${HOME}/.gitignore"
+ln -sfv "${PWD}/.tmux.conf"      "${HOME}/.tmux.conf"
+ln -sfv "${PWD}/.vimrc"          "${HOME}/.vimrc"
+ln -sfv "${PWD}/.gemrc"          "${HOME}/.gemrc"
+ln -sfv "${PWD}/.irbrc"          "${HOME}/.irbrc"
+ln -sfv "${PWD}/.hyper.linux.js" "${HOME}/.hyper.js"
 
 if [[ $(uname -r) =~ Microsoft ]]; then
   WINUSER=$(/mnt/c/Windows/System32/whoami.exe | awk -F'\' '{print $2}' | tr -cd [a-z\.])
   WINHOME="/mnt/c/Users/$WINUSER"
-
-  cp -f "$cwd/.vimrc" "$WINHOME/.vimrc"
   cp -f "$cwd/.hyper.windows.js" "$WINHOME/AppData/Roaming/Hyper/.hyper.js"
-else
-  symlink "$cwd/.hyper.linux.js" "$HOME/.hyper.js"
 fi
 
-if confirm 'Install rbenv?'; then
-  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-  git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-fi
+mkdir -p "${HOME}/bin"
+source "${HOME}/.bash_profile"
 
-if confirm 'Install go binaries?'; then
-  installed curl
+install_package 'vim'
+install_package 'curl'
+install_package 'unzip'
 
-  source "$HOME/.bash_profile"
-  type go   >/dev/null 2>&1 || (curl -L https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz | sudo tar xz -C /usr/local)
-  type dep  >/dev/null 2>&1 || (curl -L https://raw.githubusercontent.com/golang/dep/master/install.sh | sh)
-fi
+type go     >/dev/null 2>&1 || ( curl -L https://dl.google.com/go/${GO_VERSION}.tar.gz | sudo tar xz -C /usr/local )
+curl -L https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh -o "${HOME}/.git-prompt.sh"
 
-if confirm 'Install vim plugins?'; then
-  installed vim
-  installed curl
+type vold   >/dev/null 2>&1 || ( go get -u github.com/vim-volt/volt )
+type ghq    >/dev/null 2>&1 || ( go get -u github.com/motemen/ghq )
+type memo   >/dev/null 2>&1 || ( go get -u github.com/mattn/memo )
+type peco   >/dev/null 2>&1 || ( go get -u github.com/peco/peco )
 
-  type volt >/dev/null 2>&1 || (curl -L https://github.com/vim-volt/volt/releases/download/v0.3.2/volt-v0.3.2-linux-amd64 -o "$HOME/bin/volt" && chmod a+x "$HOME/bin/volt")
+volt get -u tomasr/molokai
+volt get -u fatih/vim-go
+volt get -u vim-ruby/vim-ruby
+volt get -u airblade/vim-gitgutter
+volt get -u bronson/vim-trailing-whitespace
+volt get -u thinca/vim-quickrun
+volt get -u tpope/vim-markdown
+volt get -u itchyny/lightline.vim
+volt get -u justinmk/vim-dirvish
 
-  $HOME/bin/volt get -u tomasr/molokai
-  $HOME/bin/volt get -u fatih/vim-go
-  $HOME/bin/volt get -u vim-ruby/vim-ruby
-  $HOME/bin/volt get -u airblade/vim-gitgutter
-  $HOME/bin/volt get -u bronson/vim-trailing-whitespace
-  $HOME/bin/volt get -u thinca/vim-quickrun
-  $HOME/bin/volt get -u tpope/vim-markdown
-  $HOME/bin/volt get -u itchyny/lightline.vim
-  $HOME/bin/volt get -u justinmk/vim-dirvish
-fi
+git clone https://github.com/sstephenson/rbenv.git "${HOME}/.rbenv"
+git clone https://github.com/sstephenson/ruby-build.git "${HOME}/.rbenv/plugins/ruby-build"
 
-if confirm 'Install other files?'; then
-  installed curl
-  installed unzip
-
-  type peco   >/dev/null 2>&1 || (curl -L https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_amd64.tar.gz | tar xz && mv -f peco_linux_amd64/peco "$HOME/bin" && rm -rf peco_linux_amd64)
-  type memo   >/dev/null 2>&1 || (curl -LO https://github.com/mattn/memo/releases/download/v0.0.4/memo_linux_amd64.zip && unzip memo_linux_amd64.zip -d "$HOME/bin" && rm -f memo_linux_amd64.zip)
-  type ghq    >/dev/null 2>&1 || (curl -LO https://github.com/motemen/ghq/releases/download/v0.8.0/ghq_linux_amd64.zip && mkdir -p _ghq && unzip ghq_linux_amd64.zip -d _ghq && mv -f _ghq/ghq "$HOME/bin" && rm -rf _ghq ghq_linux_amd64.zip)
-  type direnv > /dev/null 2>&1 || (curl -L https://github.com/direnv/direnv/releases/download/v2.20.0/direnv.linux-amd64 -o ~/bin/direnv && chmod a+x ~/bin/direnv)
-
-  curl -L https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh -o "$HOME/.git-prompt.sh"
-fi
+( cd "${HOME}/.rbenv" && git pull origin master )
+( cd "${HOME}/.rbenv/plugins/ruby-build" && git pull origin master )
