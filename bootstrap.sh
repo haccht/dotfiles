61e156387c19403d64289f957cfed0e52c9e08ca
haccht <haccht@users.noreply.github.com>
2020-04-15 15:20:39 +0900

    Fix issues with debian env

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,18 @@ GO_VERSION=go1.13.5.linux-amd64
 
 install_package() {
   pkgname=$1
-  if !type "${pkgname}" >/dev/null 2>&1; then
+  if !(type "${pkgname}" >/dev/null 2>&1); then
     echo "Installing ${pkgname}..."
-    type yum      >/dev/null 2>&1 && sudo yum install -y "${pkgname}"
     type yay      >/dev/null 2>&1 && sudo yay -S --noconfirm "${pkgname}"
+    type yum      >/dev/null 2>&1 && sudo yum install -y "${pkgname}"
+    type apt-get  >/dev/null 2>&1 && sudo apt-get update && sudo apt-get install -y "${pkgname}"
     type pacman   >/dev/null 2>&1 && sudo pacman -S --noconfirm "${pkgname}"
-    type apt-get  >/dev/null 2>&1 && sudo apt-get update && apt-get install -y
   fi
 }
+
+install_package vim
+install_package curl
+install_package unzip
 
 ln -sfv "${PWD}/bash_profile"   "${HOME}/.bash_profile"
 ln -sfv "${PWD}/bashrc"         "${HOME}/.bashrc"
@@ -26,19 +30,16 @@ ln -sfv "${PWD}/hyper.linux.js" "${HOME}/.hyper.js"
 if [[ $(uname -r) =~ Microsoft ]]; then
   WINUSER=$(/mnt/c/Windows/System32/whoami.exe | awk -F'\' '{print $2}' | tr -cd [a-z\.])
   WINHOME="/mnt/c/Users/${WINUSER}"
-  cp -f "${PWD}/hyper.windows.js" "${WINHOME}/AppData/Roaming/Hyper/hyper.js"
+  cp -f "${PWD}/hyper.windows.js" "${WINHOME}/AppData/Roaming/Hyper/.hyper.js"
 fi
 
 mkdir -p "${HOME}/src"
 mkdir -p "${HOME}/bin"
 
-install_package vim
-install_package curl
-install_package unzip
-
 
 (
     source "${HOME}/.bash_profile"
+    export GHG_HOME="$HOME"
 
     [[ -f "${GOROOT}/bin/go"       ]] || ( curl -L https://dl.google.com/go/${GO_VERSION}.tar.gz | sudo tar xz -C /usr/local )
     [[ -f "${HOME}/.git-prompt.sh" ]] || ( curl -L https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh -o "${HOME}/.git-prompt.sh" )
