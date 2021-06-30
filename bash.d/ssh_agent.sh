7b2025c88f654c5bce59e830f5b65df746e5948a
haccht <haccht@users.noreply.github.com>
2021-06-30 18:22:31 +0900

    Minor update

--- bash.d/ssh_agent.sh
+++ bash.d/ssh_agent.sh
@@ -5,7 +5,12 @@ function start_agent {
     /usr/bin/ssh-agent > "$SSH_AGENT_FILE"
     chmod 600 "$SSH_AGENT_FILE"
     source "$SSH_AGENT_FILE" > /dev/null
-    /usr/bin/ssh-add -l >& /dev/null || /usr/bin/ssh-add
+    /usr/bin/ssh-add -l >& /dev/null || {
+        /usr/bin/ssh-add $HOME/.ssh/github.key
+        /usr/bin/ssh-add $HOME/.ssh/haccht.key
+        /usr/bin/ssh-add $HOME/.ssh/id_ecdsa
+        /usr/bin/ssh-add $HOME/.ssh/id_rsa
+    }
 }
 
 if [ -f "$SSH_AGENT_FILE" ]; then
