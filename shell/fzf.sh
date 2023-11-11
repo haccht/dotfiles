b29b72e8d8bd62aca4a12e00abd75997cfccbaaf
haccht <haccht@users.noreply.github.com>
2023-11-11 15:02:47 +0900

    Update

--- shell/fzf.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-if type git >/dev/null 2>&1 && [[ -t ]]; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
-
-    ln -s ~/.fzf/bin/fzf ~/bin/fzf
-fi
--- shell/setup.sh
+++ shell/setup.sh
@@ -8,6 +8,15 @@ if type curl >/dev/null 2>&1; then
     curl -sL https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh -o "${HOME}/.bash.d/git-prompt.sh"
 fi
 
+# install fzf if not install via package task
+if !(type fzf >/dev/null 2>&1); then
+    if type git >/dev/null 2>&1; then
+        git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf
+        ${HOME}/.fzf/install
+        ln -s ${HOME}/.fzf/bin/fzf ${HOME}/bin/fzf
+    fi
+fi
+
 # vim plugins
 if type vim >/dev/null 2>&1; then
     vim -es -u ~/.vimrc +PlugUpgrade +PlugInstall +PlugUpdate +PlugClean! +qall
